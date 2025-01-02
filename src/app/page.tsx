c0b2f39f47156ad6eef064d5c04b5c14e9896888
SupaHacka02 <mherericgyulumyan@gmail.com>
2025-01-01 23:43:24 -0400

    Initial Set-Up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,160 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import React, { useState } from 'react';
+
+const TicTacToe = () => {
+
+  const [turn, setTurn] = useState<string>("blue");
+
+  const [winnerStatus, setWinnerStatus] = useState<boolean>(false);
+  const [winner, setWinner] = useState<string>(turn);
+
+  const [squares, setSquares] = useState([
+    { x: 0, y: 0, fill: "white"},
+    { x: 1, y: 0, fill: "white"},
+    { x: 2, y: 0, fill: "white"},
+
+    { x: 0, y: 1, fill: "white"},
+    { x: 1, y: 1, fill: "white"},
+    { x: 2, y: 1, fill: "white"},
+
+    { x: 0, y: 2, fill: "white"},
+    { x: 1, y: 2, fill: "white"},
+    { x: 2, y: 2, fill: "white"}
+  ]);
+  
+  const onSquareClick = (index: number) => {
+    if(!winnerStatus && squares[index].fill == "white") {
+      const newSquares = [...squares];
+      newSquares[index].fill = turn;
+      setSquares(newSquares);
+      if(checkWinner()) {
+        setWinnerStatus(true);
+        setWinner(turn);
+      } else {
+        switchTurn();
+      }
+    }
+  }
+
+  const switchTurn = () => {
+    setTurn(prevTurn => prevTurn === "blue" ? "red" : "blue");
+  }
+
+  const checkWinner = () => {
+    const winningPossibilities = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for(let possibility of winningPossibilities) {
+      const [a, b, c] = possibility;
+      if(squares[a].fill !== "white" && squares[a].fill === squares[b].fill && squares[b].fill === squares[c].fill) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function reloadPage() { 
+    window.location.reload(); 
+  }
+
+  function resetGame() {
+    const newSquares = [...squares];
+    for(let i = 0; i < squares.length; i++) {
+      newSquares[i].fill = "white";
+    }
+    setSquares(newSquares);
+    setTurn("blue");
+    setWinnerStatus(false);
+    setWinner(turn);
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+        <div>
+          <h1
+            style={{
+              position: 'absolute',
+              top: '20%',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              fontSize: '28px',
+              color: 'white',
+            }}>
+            Welcome to Competitive Tic-Tac-Toe!
+          </h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(3, 100px)',
+            gridTemplateRows: 'repeat(3, 100px)',
+            gap: '10px',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}>
+          {squares.map((square, index) => (
+            <div 
+              key={index}
+              className='square'
+              style={{
+                backgroundColor: square.fill,
+                border: '2px solid black',
+                cursor: 'pointer',
+              }}
+              onClick={() => onSquareClick(index)}
+            />
+          ))}
+          </div>
+        <div>
+          {
+            winnerStatus &&
+            <h1
+              style={{
+                position: 'absolute',
+                bottom: '20%',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                fontSize: '16px',
+                color: 'white',
+              }}>
+              Winner: {turn}!
+            </h1>
+          }
+        </div>
+        <div>
+          <button
+            onClick={resetGame}
+            style={{
+              position: 'absolute',
+              bottom: '10%',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              padding: '10px 20px',
+              fontSize: '16px',
+              backgroundColor: '#b642f5',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}>
+            Reset
+          </button>  
+        </div>
+    </>
   );
 }
+
+export default TicTacToe;
